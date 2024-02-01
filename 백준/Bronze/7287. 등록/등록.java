31987b8d308eb9b6f93403ba505b966bbf3332d5
Joongwon Lee <77660935+joongwonLee@users.noreply.github.com>
2024-02-01 21:13:10 +0900

    [Bronze V] Title: 등록, Time: 68 ms, Memory: 11188 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println(289);
+        System.out.println("wnddnjs823");
+    }
+}
