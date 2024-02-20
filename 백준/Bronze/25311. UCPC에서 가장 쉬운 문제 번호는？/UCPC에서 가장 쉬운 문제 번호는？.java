444509d8a36e365421ca410955c50d6bf1a4694c
Joongwon Lee <77660935+joongwonLee@users.noreply.github.com>
2024-02-20 14:35:29 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 80 ms, Memory: 11456 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    static BufferedReader br;
+    static String year;
+    
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        
+        year = br.readLine();
+        System.out.println("A");
+    }
+}
