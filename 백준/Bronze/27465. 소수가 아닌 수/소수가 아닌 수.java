a9adaa2316c832616da8b42ae5aed2f98d72ff18
Joongwon Lee <77660935+joongwonLee@users.noreply.github.com>
2024-03-08 09:20:18 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 76 ms, Memory: 11508 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
@@ -4,12 +4,12 @@ import java.io.*;
 public class Main {
     
     static BufferedReader br;
-    static int N;
+    static long N;
     
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         
-        N = Integer.parseInt(br.readLine());
+        N = Long.parseLong(br.readLine());
         
         while(true) {
             if(!isPrime(N)) {
@@ -21,15 +21,17 @@ public class Main {
         }
     }
     
-    private static boolean isPrime(int num) {
+    private static boolean isPrime(long num) {
         
         if(num==1) return false;
         
         for(int i=2; i<=Math.sqrt(num); i++) {
             
-            for(int j=i*2; j<=num; j+=i) {
-                if(num%j==0) return false;
-            }
+            if(num%i==0) return false;
+            
+//            for(int j=i*2; j<num; j+=i) {
+//                if(num%j==0) return false;
+//            }
         }
         
         return true;
