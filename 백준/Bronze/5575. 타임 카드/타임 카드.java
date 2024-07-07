5d05b1b4e1852582ceb7a646c6a6746ec417a44e
Joongwon Lee <77660935+joongwonLee@users.noreply.github.com>
2024-07-07 15:34:52 +0900

    [Bronze IV] Title: 타임 카드, Time: 72 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/5575. 타임 카드/타임 카드.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i = 0; i < 3; i++) {
+			int time = 0;
+			int time2 = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 2; j++) {
+				int h = Integer.parseInt(st.nextToken());
+				int m = Integer.parseInt(st.nextToken());
+				int s = Integer.parseInt(st.nextToken());
+				if(j == 0) {
+					time = (h * 3600) + (m * 60) + s;					
+				}else {
+					time2 = (h * 3600) + (m * 60) + s;
+				}
+			}
+			time2 -= time;
+			System.out.println(time2 / 3600 + " " + (time2 % 3600) / 60 + " " + (time2 % 3600) % 60);
+		}
+	}
+}
