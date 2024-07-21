0ffdbf4d7b751918a48b2eade56e30d8ede55277
Joongwon Lee <77660935+joongwonLee@users.noreply.github.com>
2024-07-21 21:57:36 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int d1 = Integer.parseInt(br.readLine());
+        int d2 = Integer.parseInt(br.readLine());
+        
+        System.out.println(d1*2 + (2*3.141592*d2));
+    }
+}
