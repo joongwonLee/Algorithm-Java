5060792285f3a1a5a6fc8b6528a5e4b94a94edb6
Joongwon Lee <77660935+joongwonLee@users.noreply.github.com>
2024-08-19 00:08:43 +0900

    [Bronze III] Title: 접시 안의 원, Time: 64 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        System.out.println(Math.round(Math.pow((double)T/2,2)));
+    }
+}
